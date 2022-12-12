8f4c339fbcb7ed3ac76f6f8da0283eb9431759d8
Lars Kinkel <larskinkel@sp-byods-145-109-32-223.wireless.uva.nl>
2022-12-12 13:38:47 +0100

    laatste status

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("css", views.css, name="css"),
     path("<str:entry>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,20 +1,25 @@
+import markdown2
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label = 'Search Encyclopedia')
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
-def css(request):
-    return render(request, "encyclopedia/css.html", {
-        "info": util.get_entry("CSS")
+        "entries": util.list_entries(),
+        "form" : NewTaskForm()
     })
 
 def entry(request, entry):
-    return render(request, "encyclopedia/entries.html",{
-        "entryname": entry,
-        "entry": util.get_entry(f"{entry}")
-    })
+    if request.method == "POST":
+        searched = 
+
+    if entry in util.list_entries():
+        return render(request, "encyclopedia/entries.html",{
+            "entryname": entry,
+            "entry": markdown2.markdown(util.get_entry(f"{entry}"))
+            })
+    else:
+        return render(request, "encyclopedia/error.html")
