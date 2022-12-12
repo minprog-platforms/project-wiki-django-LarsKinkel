368f31dbd3db49259d7f6d9760e1ff1b38d04654
Lars Kinkel <larskinkel@sp-byods-145-109-32-223.wireless.uva.nl>
2022-12-12 18:22:23 +0100

    Afmaken search gedeelte van de website, new files hiervoor aangemaakt: searchentry.html en searchresults.html. Toevoegen van nieuwe path. Ook de module create new page is u afgerond, nieuwe files hiervoor: createnew.html, Amsterdam Science Park.md

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry>", views.entry, name="entry")
+    path("<str:entry>", views.entry, name="entry"),
+    path("searched/", views.searchentry, name="searchentry"),
+    path("createnew/", views.createnew, name="createnew"),
+    path("createdpage/", views.createdpage, name="createdpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,23 +3,72 @@ from django.shortcuts import render
 from django import forms
 from . import util
 
-class NewTaskForm(forms.Form):
-    task = forms.CharField(label = 'Search Encyclopedia')
+class SearchEntryForm(forms.Form):
+    entry_searched = forms.CharField(label = "")
+
+class CreatePageForm(forms.Form):
+    title = forms.CharField(label = "Title")
+    content = forms.CharField(widget=forms.Textarea(), label = "Markdown content for the page")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "form" : NewTaskForm()
+        "form" : SearchEntryForm()
     })
 
 def entry(request, entry):
-    if request.method == "POST":
-        searched = 
-
     if entry in util.list_entries():
         return render(request, "encyclopedia/entries.html",{
-            "entryname": entry,
-            "entry": markdown2.markdown(util.get_entry(f"{entry}"))
+            "entry": markdown2.markdown(util.get_entry(f"{entry}")),
+            "form" : SearchEntryForm()
             })
     else:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "form" : SearchEntryForm()
+            })
+
+def searchentry(request):
+    if request.method == "POST":
+        entry = SearchEntryForm(request.POST)
+        if entry.is_valid():
+            searched = entry.cleaned_data['entry_searched']
+            if searched in util.list_entries():
+                return render(request, "encyclopedia/entries.html",{
+                    "entry": markdown2.markdown(util.get_entry(f"{searched}")),
+                    "form" : SearchEntryForm()
+                    })
+            else:
+                results = []
+                for i in range(len(util.list_entries())):
+                    if searched in util.list_entries()[i]:
+                        results.append(util.list_entries()[i])
+                return render(request, "encyclopedia/searchresults.html", {
+                    "results": results,
+                    "form" : SearchEntryForm()
+                })
+
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries(),
+            "form" : SearchEntryForm()
+        })
+
+def createnew(request):
+    return render(request, "encyclopedia/createnew.html", {
+        "form" : SearchEntryForm(),
+        "title": CreatePageForm()
+
+        })
+
+def createdpage(request):
+    if request.method == "POST":
+        page = CreatePageForm(request.POST)
+        if page.is_valid():
+            title = page.cleaned_data['title']
+            content = page.cleaned_data['content']
+            util.save_entry(title, content)
+
+        return render(request, "encyclopedia/entries.html",{
+            "entry": markdown2.markdown(util.get_entry(f"{title}")),
+            "form" : SearchEntryForm()
+            })
